f42e123ea9d0fb732734b54eed4d87e012cdd8a8
Shriharsha Konda <shkkonda@gmail.com>
2023-06-22 01:24:41 +0530

    ensure different images

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ import streamlit as st
 import psycopg2
 import pandas as pd
 import random
+from typing import List, Tuple
 
 CSV_URL = "https://raw.githubusercontent.com/shkkonda/imageEloCalc/main/nokiamon_image.csv"
 final_df = pd.read_csv(CSV_URL)
 
-def get_random_image(df) -> str:
+def get_random_image_pair(df) -> Tuple[str, str]:
     left_image = random.choice(df['image_link'])
-    return left_image
+    right_image = random.choice(df['image_link'])
+
+    while left_image == right_image:
+        right_image = random.choice(df['image_link'])
+
+    return left_image, right_image
+
+left_image, right_image = get_random_image(final_df)
 
 @st.cache_resource
 def get_database_connection():
@@ -25,30 +33,28 @@ col1, col2 = st.columns(2)
 
 # Form 1 in the first column
 with col1.form("my_form"):
-    random_image = get_random_image(final_df)
     image_link = st.empty()
-    image_link.image(random_image)
+    image_link.image(left_image)
     submit = st.form_submit_button("Submit")
 
 # Form 2 in the second column
 with col2.form("my_form_2"):
-    random_image_2 = get_random_image(final_df)
     image_link_2 = st.empty()
-    image_link_2.image(random_image_2)
+    image_link_2.image(right_image)
     submit_2 = st.form_submit_button("Submit")
 
 # If the submit button of Form 1 is clicked, write the data to the database
 if submit:
     cursor = get_database_connection().cursor()
     query = "INSERT INTO images (image_link) VALUES (%s)"
-    cursor.execute(query, (random_image,))
+    cursor.execute(query, (left_image,))
     get_database_connection().commit()
 
 # If the submit button of Form 2 is clicked, write the data to the database
 if submit_2:
     cursor = get_database_connection().cursor()
     query = "INSERT INTO images (image_link) VALUES (%s)"
-    cursor.execute(query, (random_image_2,))
+    cursor.execute(query, (right_image,))
     get_database_connection().commit()
 
 # Display a message
