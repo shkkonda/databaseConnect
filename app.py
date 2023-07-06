2b0e07cbac0c558606c4cf44c50205d81f130533
Shriharsha Konda <shkkonda@gmail.com>
2023-07-06 20:05:17 +0530

    stop at 10 choices

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ def get_database_connection():
     conn = psycopg2.connect(**st.secrets["postgres"])
     return conn
 
+def display_thank_you_note():
+    st.write("Thank you for your submissions! You've been a great Nokiamon judge!")
+
 # Counter to keep track of submissions
 if "submission_counter" not in st.session_state:
     st.session_state.submission_counter = 0
@@ -97,112 +100,117 @@ if menu_selection == "Ranks":
 
     
 else:
-    messages = [
-        "Great choice!",
-        "Well done!",
-        "Fantastic!",
-        "Awesome selection!",
-        "You've got good taste!",
-        "Impressive choice!",
-        "You're on a roll!",
-        "Excellent decision!",
-        "Way to go!",
-        "You're a Nokiamon master!",
-        "Outstanding selection!",
-        "That's a winner!",
-        "You know your Nokiamon!",
-        "Amazing pick!",
-        "That's a rare Nokiamon!"
-    ]
+    if st.session_state.submission_counter >= 10:
+        display_thank_you_note()
+    else:
+        messages = [
+            "Great choice!",
+            "Well done!",
+            "Fantastic!",
+            "Awesome selection!",
+            "You've got good taste!",
+            "Impressive choice!",
+            "You're on a roll!",
+            "Excellent decision!",
+            "Way to go!",
+            "You're a Nokiamon master!",
+            "Outstanding selection!",
+            "That's a winner!",
+            "You know your Nokiamon!",
+            "Amazing pick!",
+            "That's a rare Nokiamon!"
+        ]
+    
+        @st.cache_data  # Cache the DataFrame
+        def fetch_table_as_dataframe():
+            conn = get_database_connection()
+            cursor = conn.cursor()
+    
+            # Execute the query to fetch the table
+            cursor.execute("SELECT * FROM noki_combos")
+    
+            # Fetch all the rows
+            rows = cursor.fetchall()
+    
+            # Get the column names
+            columns = [desc[0] for desc in cursor.description]
+    
+            # Create a DataFrame with the fetched data
+            df = pd.DataFrame(rows, columns=columns)
+    
+            return df
+    
+        # Usage
+        noki_combos_df = fetch_table_as_dataframe()
+    
+        def get_random_images(df):
+            # Get a random row from the DataFrame
+            random_row = df.sample(n=1, random_state=random.seed())
+    
+            # Extract the values from the 'noki_1' and 'noki_2' columns
+            left_image = random_row['noki_1'].values[0]
+            right_image = random_row['noki_2'].values[0]
+    
+            # Return the left_image and right_image
+            return left_image, right_image
+    
+        left_image, right_image = get_random_images(noki_combos_df)
+    
+        if 'left_image' not in st.session_state:
+            st.session_state.left_image = left_image
+        if 'right_image' not in st.session_state:
+            st.session_state.right_image = right_image
+    
+        wallet_address = st.text_input('Enter Wallet Address (Optional - Add if you want be considered for free noki airdrops)')  # New Field for Wallet Address
+    
+        # Create a grid layout with two columns
+        col1, col2 = st.columns(2)
+    
+        # Form 1 in the first column
+        with col1.form("my_form"):
+            image_link = st.empty()
+            image_link.image(left_image, use_column_width=True)
+            form_submit = st.form_submit_button("Submit")
+    
+        # Form 2 in the second column
+        with col2.form("my_form_2"):
+            image_link_2 = st.empty()
+            image_link_2.image(right_image, use_column_width=True)
+            form_submit_2 = st.form_submit_button("Submit")
+    
+        # If the submit button of Form 1 is clicked, write the data to the database
+        if form_submit:
+            cursor = get_database_connection().cursor()
+            query = "INSERT INTO noki_selections_dev (left_image_link, right_image_link, selected_image_link, wallet_address) VALUES (%s, %s, %s, %s)"
+            cursor.execute(query, (st.session_state.left_image, st.session_state.right_image, st.session_state.left_image, wallet_address))
+            get_database_connection().commit()
+            st.session_state.left_image = left_image
+            st.session_state.right_image = right_image
+            # Increment the submission counter after each submission
+            st.session_state.submission_counter += 1
+            # Display a randomly selected success message
+            success_message = random.choice(messages)
+            st.write(success_message)
+    
+        # If the submit button of Form 2 is clicked, write the data to the database
+        if form_submit_2:
+            cursor = get_database_connection().cursor()
+            query = "INSERT INTO noki_selections_dev (left_image_link, right_image_link, selected_image_link, wallet_address) VALUES (%s, %s, %s, %s)"
+            cursor.execute(query, (st.session_state.left_image, st.session_state.right_image, st.session_state.right_image, wallet_address))
+            get_database_connection().commit()
+            st.session_state.left_image = left_image
+            st.session_state.right_image = right_image
+            # Increment the submission counter after each submission
+            st.session_state.submission_counter += 1
+            # Display a randomly selected success message
+            success_message = random.choice(messages)
+            st.write(success_message)
+    
+        # Update session state variables on any non-submit interaction
+        if not form_submit and not form_submit_2:
+            st.session_state.left_image = left_image
+            st.session_state.right_image = right_image
 
-    @st.cache_data  # Cache the DataFrame
-    def fetch_table_as_dataframe():
-        conn = get_database_connection()
-        cursor = conn.cursor()
-
-        # Execute the query to fetch the table
-        cursor.execute("SELECT * FROM noki_combos")
-
-        # Fetch all the rows
-        rows = cursor.fetchall()
-
-        # Get the column names
-        columns = [desc[0] for desc in cursor.description]
-
-        # Create a DataFrame with the fetched data
-        df = pd.DataFrame(rows, columns=columns)
-
-        return df
-
-    # Usage
-    noki_combos_df = fetch_table_as_dataframe()
-
-    def get_random_images(df):
-        # Get a random row from the DataFrame
-        random_row = df.sample(n=1, random_state=random.seed())
-
-        # Extract the values from the 'noki_1' and 'noki_2' columns
-        left_image = random_row['noki_1'].values[0]
-        right_image = random_row['noki_2'].values[0]
-
-        # Return the left_image and right_image
-        return left_image, right_image
-
-    left_image, right_image = get_random_images(noki_combos_df)
-
-    if 'left_image' not in st.session_state:
-        st.session_state.left_image = left_image
-    if 'right_image' not in st.session_state:
-        st.session_state.right_image = right_image
-
-    wallet_address = st.text_input('Enter Wallet Address (Optional - Add if you want be considered for free noki airdrops)')  # New Field for Wallet Address
-
-    # Create a grid layout with two columns
-    col1, col2 = st.columns(2)
-
-    # Form 1 in the first column
-    with col1.form("my_form"):
-        image_link = st.empty()
-        image_link.image(left_image, use_column_width=True)
-        form_submit = st.form_submit_button("Submit")
-
-    # Form 2 in the second column
-    with col2.form("my_form_2"):
-        image_link_2 = st.empty()
-        image_link_2.image(right_image, use_column_width=True)
-        form_submit_2 = st.form_submit_button("Submit")
-
-    # If the submit button of Form 1 is clicked, write the data to the database
-    if form_submit:
-        cursor = get_database_connection().cursor()
-        query = "INSERT INTO noki_selections_dev (left_image_link, right_image_link, selected_image_link, wallet_address) VALUES (%s, %s, %s, %s)"
-        cursor.execute(query, (st.session_state.left_image, st.session_state.right_image, st.session_state.left_image, wallet_address))
-        get_database_connection().commit()
-        st.session_state.left_image = left_image
-        st.session_state.right_image = right_image
-        # Display a randomly selected success message
-        success_message = random.choice(messages)
-        st.write(success_message)
-
-    # If the submit button of Form 2 is clicked, write the data to the database
-    if form_submit_2:
-        cursor = get_database_connection().cursor()
-        query = "INSERT INTO noki_selections_dev (left_image_link, right_image_link, selected_image_link, wallet_address) VALUES (%s, %s, %s, %s)"
-        cursor.execute(query, (st.session_state.left_image, st.session_state.right_image, st.session_state.right_image, wallet_address))
-        get_database_connection().commit()
-        st.session_state.left_image = left_image
-        st.session_state.right_image = right_image
-        # Display a randomly selected success message
-        success_message = random.choice(messages)
-        st.write(success_message)
-
-    # Update session state variables on any non-submit interaction
-    if not form_submit and not form_submit_2:
-        st.session_state.left_image = left_image
-        st.session_state.right_image = right_image
-
-    # Increment the submission counter after each submission
-    st.session_state.submission_counter += 1
     st.write(st.session_state.submission_counter)
 
 
