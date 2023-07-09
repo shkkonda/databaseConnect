641fcaf37bd7cbf0203be8cc77d6b18afed29863
Shriharsha Konda <shkkonda@gmail.com>
2023-07-09 11:02:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -169,9 +169,9 @@ else:
             st.session_state.right_image = right_image
     
         if "wallet_address" not in st.session_state:
-            st.session_state.wallet_address = ''
+            st.session_state.wallet_address = None
         
-        wallet_address = st.text_input('Enter Wallet Address (Optional - Add if you want to be considered for free Noki airdrops)', value=st.session_state.wallet_address)
+        wallet_address = st.text_input('Enter Wallet Address (Optional - Add if you want to be considered for free Noki airdrops)')
         st.session_state.wallet_address = wallet_address
 
     
